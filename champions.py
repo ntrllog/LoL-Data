3855e32157105c5b79afc41b016c23206dbf725b
alan-lam <alanlam00@gmail.com>
2020-08-07 15:29:25 -0700

    convert id to display name

--- champions.py
+++ champions.py
@@ -1,18 +1,15 @@
 import requests, json
 
-res = requests.get('http://ddragon.leagueoflegends.com/cdn/10.6.1/data/en_US/champion.json')
+res = requests.get('http://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/champion.json')
 
 id_dict = json.loads(res.text)
 
-###
-# In champion.json, the "data" key maps to a dictionary where each key* is the champion name and the value is a dictionary containing information about the champion. 
-###
-
-# x is the key* (champion name)
 ids = list(map(lambda x: int(id_dict['data'][x]['key']), id_dict['data']))
+names = list(map(lambda x: id_dict['data'][x]['name'], id_dict['data']))
 
-idMap = dict(zip(ids, id_dict['data'].keys()))
+idMap = dict(zip(ids, names))
 
 jsFile = open('champions.js', 'w')
-jsFile.write('const champions = ' + str(idMap) + ';\n')
+jsFile.write('const idToChampNameMap = ' + str(idMap) + ';\n')
+jsFile.write('\nconst idToChampKeyMap = ' + str(dict(zip(ids, id_dict['data'].keys()))) + ';\n')
 jsFile.close()
