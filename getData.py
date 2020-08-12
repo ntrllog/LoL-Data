92e8480dd3ff3b74ff6312a9c7625c937149ed53
alan-lam <alanlam00@gmail.com>
2020-08-11 17:49:47 -0700

    add runes

--- getData.py
+++ getData.py
@@ -16,15 +16,23 @@ spells = json.loads(res.text)
 spell_ids = list(map(lambda x: int(spells['data'][x]['key']), spells['data']))
 spell_image_urls = list(map(lambda x: spells['data'][x]['image']['full'], spells['data']))
 
-res = requests.get(f'https://ddragon.leagueoflegends.com/cdn/{PATCH_VERSION}/data/en_US/item.json')
+with open('runesReforged.json') as f:
+    runes = json.load(f)
 
-items = json.loads(res.text)
+rune_path_ids = list(map(lambda x: x['id'], runes))
+rune_path_names = list(map(lambda x: x['key'], runes))
 
-item_image_urls = list(map(lambda x: items['data'][x]['image']['full'], items['data']))
+keystone_ids = []
+keystone_names = []
 
-jsFile = open('gameData.js', 'w')
-jsFile.write('const idToChampNameMap = ' + str(dict(zip(champion_ids, names))) + ';\n')
-jsFile.write('\nconst idToChampKeyMap = ' + str(dict(zip(champion_ids, champions['data'].keys()))) + ';\n')
-jsFile.write('\nconst idToSpellImageUrlMap = ' + str(dict(zip(spell_ids, spell_image_urls))) + ';\n')
-jsFile.write('\nconst idToItemImageUrlMap = ' + str(dict(zip(items['data'].keys(), item_image_urls))) + ';\n')
-jsFile.close()
+for rune_path in runes:
+    for keystone in rune_path['slots'][0]['runes']:
+        keystone_ids.append(keystone['id'])
+        keystone_names.append(keystone['key'])
+
+with open('gameData.js', 'w') as jsFile:
+    jsFile.write('const idToChampNameMap = ' + str(dict(zip(champion_ids, names))) + ';\n')
+    jsFile.write('\nconst idToChampKeyMap = ' + str(dict(zip(champion_ids, champions['data'].keys()))) + ';\n')
+    jsFile.write('\nconst idToSpellImageUrlMap = ' + str(dict(zip(spell_ids, spell_image_urls))) + ';\n')
+    jsFile.write('\nconst idToRunePathNameMap = ' + str(dict(zip(rune_path_ids, rune_path_names))) + ';\n')
+    jsFile.write('\nconst idToKeystoneNameMap = ' + str(dict(zip(keystone_ids, keystone_names))) + ';\n')
